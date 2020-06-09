e69d80f4d77c98165bcb4271dfb83d58653a6401
ahmedBou <instinctauto@gmail.com>
2020-06-09 01:43:58 +0100

    create db and table users, connect server with db with pdo and fetch some data

---
+++ pdo-mysql/auth.php
@@ -0,0 +1,21 @@
+<?php 
+// echo "<pre>\n";
+
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=misc','root','root');
+$stmt = $pdo->query("SELECT name,email,PASSWORD FROM users");
+
+echo('<table border="1">'."\n");
+while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
+    // print_r($row);
+    echo ("<tr><td>");
+    echo ($row['name']);
+    echo ("</td><td>");
+    echo ($row['email']);
+    echo ("</td><td>");
+    echo ($row['PASSWORD']);
+    echo ("</td></tr>\n");
+}
+echo "</table>"."\n";
+// echo "<pre>\n";
+
+?>
